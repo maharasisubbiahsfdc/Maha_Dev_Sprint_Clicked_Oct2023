39598731db2f5670a18a6372a690d03afca92274
Maharasi Subbiah <maharasisubbiahsfdc@gmail.com>
2023-10-29 22:17:39 -0700

    commit changes

---
+++ force-app/main/default/lwc/sendBillEmail/sendBillEmail.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+import { CurrentPageReference } from "lightning/navigation";
+
+
+export default class SendBillEmail extends LightningElement {
+
+    @api recordId;
+    @api objectApiName;
+
+    
+}
